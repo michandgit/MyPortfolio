8a8f8b23f6cce0e034f34ad63985abe17ac3abe1
michandgit <chandsigola@gmail.com>
2023-06-15 22:12:26 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// console.log("hello")
+// document.querySelector('.cross').style.display='none';
+// document.querySelector('.ham').addEventListener("click" , ()=>{
+//     document.querySelector('.sidebar').classList.toggle('sidebarGo');
+//     if (DocumentTimeline.querySelector('.sidebar').classList.contains('.sidebarGo')){
+//         document.querySelector('.ham').style.display='inline';
+//         document.querySelector('.cross').style.display='none';
+//     }else{
+//         document.querySelector('.cross').style.display='inline';
+//         document.querySelector('.ham').style.display='none';
+//     }
+// });
